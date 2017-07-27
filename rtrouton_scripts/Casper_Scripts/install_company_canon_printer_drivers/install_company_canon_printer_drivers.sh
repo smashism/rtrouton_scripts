0cf414f9cd0c96ae69ac937d940ddc9d38aa731a
emily <hellomodtitan@gmail.com>
2017-07-27 12:51:11 -0500

    Update install_company_canon_printer_drivers.sh
    After further testing the version I provided previously was not installing the printer drivers if the drivers were not installed at all, likely a formatting/globbing issue (even though the version checking with the driver present was working fine). So I made a further modification that uses if/elif rather than a series of if statements and replaced the previous formatting. I have tested this on 10.12.5 with a machine with no drivers installed, a newer version installed, and an older version installed, and all of the behavior is as expected.

--- rtrouton_scripts/Casper_Scripts/install_company_canon_printer_drivers/install_company_canon_printer_drivers.sh
+++ rtrouton_scripts/Casper_Scripts/install_company_canon_printer_drivers/install_company_canon_printer_drivers.sh
@@ -13,29 +13,23 @@ installed_driver=$(defaults read "/Library/Printers/Canon/CUPSPS2/Utilities/Cano
 # on the JSS
 
 driver_version="$4"
-dialog="The needed Canon printer drivers have not been detected. Installing Canon PS $driver_version Print Drivers before adding the requested printer."
-description=$(echo "$dialog")
+dialog="The needed Canon printer drivers have not been detected. Installing Canon PS $driver_version Print Drivers before adding the requested printer. This may take a moment."
+description=`echo "$dialog"`
 button1="OK"
 jamfHelper="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
 icon="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertNoteIcon.icns"
 
-if [ $installed_driver > $driver_version ]; then
+if [[ ${installed_driver} > ${driver_version} ]]; then
   echo "Canon PS $installed_driver Print Drivers installed"
-fi
-
-if [ $installed_driver == $driver_version ]; then
+elif [[ ${installed_driver} == ${driver_version} ]]; then
   echo "Canon PS $driver_version Print Drivers installed"
-fi
-
-if [ $installed_driver < $driver_version ]; then
+elif [[ ${installed_driver} < ${driver_version} ]]; then
   echo "Canon PS $driver_version Print Drivers not installed. Installing Canon PS $driver_version Print Drivers"
   if [[ ${osvers} -lt 7 ]]; then
 
     "$jamfHelper" -windowType utility -description "$description" -button1 "$button1" -icon "$icon" -timeout 20
 
-  fi
-
-  if [[ ${osvers} -ge 7 ]]; then
+  elif [[ ${osvers} -ge 7 ]]; then
 
     jamf displayMessage -message "$dialog"
 
